85f5a2f5b115ed560780921dccdbb7e18484993b
Perunjashka <natastadnyk88@gmail.com>
2023-09-02 20:11:16 +0300

    Home work 1

---
+++ pages/account.js
@@ -0,0 +1,35 @@
+const { I } = inject();
+
+module.exports = {
+  h1: { xpath: '//*[@id="content"]/h1'},
+  firstNameField: { xpath: '//*[@id="input-firstname"]'},
+  lastNameField: { xpath: '//*[@id="input-lastname"]'},
+  email: { xpath: '//*[@id="input-email"]'},
+  telephone: { xpath: '//*[@id="input-telephone"]'},
+  password: { xpath: '//*[@id="input-password"]'},
+  passwordConfirm: { xpath: '//*[@id="input-confirm"]'},
+  privacyPolicyButton: { xpath: '//*[@id="content"]/form/div/div/input[1]'},
+  continueButton: { xpath: '//*[@id="content"]/form/div/div/input[2]'},
+  verifyRegisterAccountPage(){
+    const regTitleText = 'Register Account';
+    I.seeTextEquals(regTitleText, this.h1);
+  },
+
+  fillNewUserForm(user){
+    I.fillField(this.firstNameField,user.firstName);
+    I.fillField(this.lastNameField,user.lastNameField);
+    I.fillField(this.email, user.email);
+    I.fillField(this.telephone, user.telephone);
+    I.fillField(this.password, user.password);
+    I.fillField(this.passwordConfirm, user.passwordConfirm);
+  },
+  submitNewUserForm(){
+    I.click(this.privacyPolicyButton);
+    I.click(this.continueButton);
+  },
+  verifySuccessfullRegistration(){
+    const accountTitleText = 'Your Account Has Been Created!';
+    I.seeTextEquals(accountTitleText, this.h1);
+  }
+
+}
